cecc583db85429d2735a977bbc7333b821334659
Semen Syrovatskiy <semen.syrovatskiy@gmail.com>
2021-04-02 21:19:55 +0300

    Add page not found route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import CV from '../views/CV.vue'
+import PageNotFound from '../views/PageNotFound.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/cv',
     name: 'CV',
     component: CV
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'PageNotFound',
+    component: PageNotFound
   }
 ]
 
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <MDBCol sm="12" md="5" lg="3" class="mt-4">
+    <h3 class="text-muted text-center">Страница не найдена 😞</h3>
+  </MDBCol>
+</template>
+
+<script>
+import { MDBCol } from 'mdb-vue-ui-kit';
+
+export default {
+  name: 'PageNotFound',
+  components: {
+    MDBCol
+  }
+};
+</script>
