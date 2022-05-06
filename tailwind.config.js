070d91723f3d9dbb09c09069c653248926b8200a
Semen Syrovatskiy <mail@syrovatskiy.tk>
2022-05-06 15:00:26 +0300

    fix(tailwind): new color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,33 @@ module.exports = {
   content: ['./src/**/*.astro'],
   theme: {
     extend: {
+      colors: {
+        index: '#881af1',
+        about: '#0d9488',
+        blog: '#ff7b72',
+        projects: '#18b3d9',
+        darknest: '#0d1117',
+      },
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            '--tw-prose-body': theme('colors.black'),
-            '--tw-prose-headings': theme('colors.black'),
-            '--tw-prose-code': theme('colors.black'),
-            '--tw-prose-counters': theme('colors.black'),
+            '--tw-prose-body': theme('colors.darknest'),
+            '--tw-prose-headings': theme('colors.blog'),
+            '--tw-prose-code': theme('colors.blog'),
+            'code::before': {
+              content: '""',
+            },
+            'code::after': {
+              content: '""',
+            },
+            '--tw-prose-counters': theme('colors.darknest'),
           },
         },
         dark: {
           css: {
             '--tw-prose-body': theme('colors.white'),
-            '--tw-prose-headings': theme('colors.white'),
-            '--tw-prose-code': theme('colors.white'),
+            '--tw-prose-headings': theme('colors.blog'),
+            '--tw-prose-code': theme('colors.blog'),
             '--tw-prose-counters': theme('colors.white'),
           },
         },
