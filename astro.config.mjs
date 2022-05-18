42098fbf2c63310d6e93e9a447fb534fff5802de
Semen Syrovatskiy <mail@syrovatskiy.tk>
2022-05-19 00:29:41 +0300

    chore: disable syntax highlight

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import tailwindcss from '@astrojs/tailwind';
 export default {
   site: 'https://syrovatskiy.tk',
   integrations: [tailwindcss()],
-  vite: {
-    ssr: {
-      external: ['svgo'], // for astro-icons
-    },
+  markdown: {
+    syntaxHighlight: false,
   },
 };
